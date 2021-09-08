e5b8149eb52d23cfa30c2997ce00f5ef7aff883c
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-09-08 16:59:50 -0300

    Refectored project

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,101 @@
+package one.digitalinnovation.personapi.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fistName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones = new ArrayList<>();
+
+
+	public Person(Long id, String fistName, String lastName, String cpf, LocalDate birthDate, List<Phone> phones) {
+		this.id = id;
+		this.fistName = fistName;
+		this.lastName = lastName;
+		this.cpf = cpf;
+		this.birthDate = birthDate;
+		this.phones = phones;
+	}
+
+	public Person() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFistName() {
+		return getFistName();
+	}
+
+	public void setFirstName(String firstName) {
+		this.fistName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public List<Phone> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(List<Phone> phones) {
+		this.phones = phones;
+	}
+    
+}
+
+
