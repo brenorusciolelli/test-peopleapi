68a1982d1ee73a9edb4f8ac7e421b5dc25c09383
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-09-14 18:12:07 -0300

    Developed get person By ID

--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.personapi.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.response.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 
@@ -41,6 +43,13 @@ public class PersonService {
 				.map(personMapper::toDTO)
 				.collect(Collectors.toList());
 				
+	}
+
+	public PersonDTO findById(Long id) throws PersonNotFoundException {
+		 Person person = personRepository.findById(id)
+				 .orElseThrow(() -> new PersonNotFoundException(id));
+		 return personMapper.toDTO(person);
+		 
 	}
     
 }
